631e186420c5e36b2dd5e38bda67a071b7f468e5
f4xm4t3 <61619636+f4xm4t3@users.noreply.github.com>
2022-03-28 17:55:20 +0200

    Add files via upload

---
+++ .github/workflows/index_1.php
@@ -0,0 +1,11 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+<?php
+phpinfo();
+phpinfo(INFO_MODULES);
+?>
